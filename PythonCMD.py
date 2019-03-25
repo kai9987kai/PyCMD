ed8c88bd0541cb6beb6097df2ffd1fc5335372ca
Jared <47715974+Jared-dareJ@users.noreply.github.com>
2019-03-25 19:21:26 +0000

    Adding PyCMD

---
+++ PythonCMD.py
@@ -0,0 +1,48 @@
+from subprocess import call
+import os
+
+print("This is a CMD EMULATOR\n    For best functionality use this on the Python Command Line")
+print("To change directory you will need to type CWD, then once prompted type the directory path. Use Forward Slashes")
+print("Default drive will be the one you launch the script in.")
+print("Some commands have benn adapted for easier use, but still give the same output.")
+print("For help type _HELP")
+print("Or my GitHub http://www.github.com/Jared-dareJ/")
+print("Example\n  CWD /Users/Jared/PycharmProjects")
+print("        By Jared (C)03/2019")
+
+def cls():
+    call('cls', shell=True)
+
+def colour(colour):
+    call('color ' + colour, shell=True)
+    print("Colour Changed To:")
+    return
+
+def acontinue():
+    print("Press Enter To Continue")
+    input()
+
+print("Press Enter To Continue")
+input("")
+
+cls()
+print("For Help Type _HELP")
+print("For normal CMD type LEGACY, Press Enter Then Type The Command")
+print("All commands displayed are case sensitive")
+acontinue()
+
+#For best emulation quality when in the command line run autoexec
+os.chdir('/users')
+call('cls', shell=True)
+print("CMD Emulator For Windows [Version 1]")
+print("    (C) Jared 2019")
+
+def CWD():
+    print("Please type the full path of the directory:")
+    ChDir = input(os.getcwd() + '> cd ')
+    os.chdir(ChDir)
+
+while 1:
+    runCOM = input(os.getcwd() + '> ')
+    if runCOM == 'CWD':
+        CWD()
